402a39049821625f77164651ed0564736c1b0c90
Chicken <chill_shyld@hotmail.com>
2018-02-01 08:03:56 +0700

    Add lecture simple and factory package code

---
+++ src/LectSimpleFactory/CheesePizza.java
@@ -0,0 +1,10 @@
+package LectSimpleFactory;
+public class CheesePizza extends Pizza {
+	public CheesePizza() {
+		name = "Cheese Pizza";
+		dough = "Regular Crust";
+		sauce = "Marinara Pizza Sauce";
+		toppings.add("Fresh Mozzarella");
+		toppings.add("Parmesan");
+	}
+}
---
+++ src/LectSimpleFactory/ClamPizza.java
@@ -0,0 +1,10 @@
+package LectSimpleFactory;
+public class ClamPizza extends Pizza {
+	public ClamPizza() {
+		name = "Clam Pizza";
+		dough = "Thin crust";
+		sauce = "White garlic sauce";
+		toppings.add("Clams");
+		toppings.add("Grated parmesan cheese");
+	}
+}
